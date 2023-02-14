8b72cebbd98c2b8ab533672395ed96e115b084c6
adgsenpai <adg@adgstudios.co.za>
2023-02-14 12:01:03 +0200

    Added User Addition

--- clockify_api_client_adgstudios/models/workspace.py
+++ clockify_api_client_adgstudios/models/workspace.py
@@ -18,3 +18,16 @@ class Workspace(AbstractClockify):
         except Exception as e:
             logging.error("API error: {0}".format(e))
             raise e
+
+    def AddUser(self, workspace_id, user_id):
+        """Add user to workspace.
+        :param workspace_id Id of workspace.
+        :param user_id      Id of user.
+        """
+        try:
+            url = self.base_url + '/workspaces/' + workspace_id + '/users/' + user_id
+            return self.post(url)
+        except Exception as e:
+            logging.error("API error: {0}".format(e))
+            raise e
+
