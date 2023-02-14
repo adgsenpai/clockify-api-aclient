10b1ad826a993206323983cea465c4277dbb4f8f
adgsenpai <adg@adgstudios.co.za>
2023-02-14 12:07:20 +0200

    Manual Version Control

--- setup.py
+++ setup.py
@@ -3,23 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("version.txt", "r") as fh:
-    version = fh.read()
-    # 1.0.4
-    # extract the last number
-    last_number = int(version.split(".")[-1])
-    # increment the last number
-    last_number += 1
-    # join the version back together
-    version = ".".join(version.split(".")[:-1]) + "." + str(last_number)    
-
-# write the new version to version.txt
-with open("version.txt", "w") as fh:
-    fh.write(version)
-
 setup(
     name='clockify-api-client-adgstudios',
-    version=version,
+    version='1.0.5',
     author="Michael Bláha, Ashlin Darius Govindasamy",
     author_email="michael.blaha@eluvia.com, adg@adgstudios.co.za",
     description="Simple python API client for clockify. Inspired by https://pypi.org/project/clockify/library.",
