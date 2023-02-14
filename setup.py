609aa503d0ed15dc4862079fca03b66f7f57c46b
adgsenpai <adg@adgstudios.co.za>
2023-02-14 11:37:39 +0200

    Patch

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 from setuptools import setup, find_packages
-import re
-import ast
-
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('clockify_api_client_adgstudios/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='clockify-api-client-adgstudios',
-    version=version,
+    version='1.0.4',
     author="Michael Bláha, Ashlin Darius Govindasamy",
     author_email="michael.blaha@eluvia.com, adg@adgstudios.co.za",
     description="Simple python API client for clockify. Inspired by https://pypi.org/project/clockify/library.",
